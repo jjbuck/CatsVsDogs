91d4696221e7e1959e9526f7bb3c1e363a5e32e0
jbuck <jonathan.j.buck@gmail.com>
2019-07-15 21:09:26 -0700

    Add logging.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, json, Response, request
 from flask_cors import CORS
+import logging
 
 import sys
 sys.path.append("..") # Adds higher directory to python modules path.
@@ -7,6 +8,8 @@ sys.path.append("..") # Adds higher directory to python modules path.
 
 app = Flask(__name__)
 CORS(app)
+
+logger = logging.getLogger(__name__)
 
 # The service basepath has a short response just to ensure that healthchecks
 # sent to the service root will receive a healthy response.
@@ -16,9 +19,21 @@ def hello_world():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
+    if request.form:
+        logger.info('Received form data.')
+        logger.info(f'Data type: {type(request.form)}')
+        url = request.get('url')
+    elif request.files:
+        logger.info('Received file data.')
+        logger.info(f'Data type: {type(request.files)}')
+        file = request.get('file', None)
+    else:
+        logger.info('Received unknown data type.')
+
     return jsonify({"message" : "Hello from /predict!",
                     "form": f'{request.form}',
-                    "file": f'{request.files}'})
+                    "file": f'{request.files}',
+                    "args": f'{request.args}'})
 
 
 # Run the service on the local server it has been deployed to,
--- tasks/build.sh
+++ tasks/build.sh
@@ -4,5 +4,5 @@ export AWS_ACCOUNT_ID="784757526031"
 export APP_NAME="CatsVsDogs"
 docker build . -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NAME_LOWERCASE}:latest
 
-# docker run -d -p 5000:5000 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NAME_LOWERCASE}:latest
+docker run -d -p 8080:8080 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NAME_LOWERCASE}:latest
 # http://localhost:8080
