0f330f1ac9af594dbcc1943e63529dc0a97ace50
jbuck <jonathan.j.buck@gmail.com>
2019-08-10 14:58:53 -0700

    Update build.sh helper script.

--- tasks/build.sh
+++ tasks/build.sh
@@ -7,9 +7,17 @@ docker build . -t ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NA
 docker run -d -p 8080:8080 ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${APP_NAME_LOWERCASE}:latest
 
 # http://localhost:8080
-# curl http://127.0.0.1:8080/predict?image_url=https://upload.wikimedia.org/wikipedia/commons/9/93/Golden_Retriever_Carlos_%2810581910556%29.jpg
-# curl -X POST -F "url=https://upload.wikimedia.org/wikipedia/commons/9/93/Golden_Retriever_Carlos_%2810581910556%29.jpg" http://127.0.0.1:8080/predict
-# curl -X POST -F "url=https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg" http://127.0.0.1:8080/predict
 
+# Test url input
+# curl -X POST -H "Content-Type: application/json" -d '{"url":"https://upload.wikimedia.org/wikipedia/commons/9/93/Golden_Retriever_Carlos_%2810581910556%29.jpg"}' http://127.0.0.1:8080/predict
+
+# Test file upload input
 # curl -X POST -F "file=@./cats_vs_dogs/tests/resources/Golden_Retriever_Carlos_(10581910556).jpg" http://127.0.0.1:8080/predict
+
+# Useful commands
+
+# Stop all running containers
 # docker stop $(docker ps -aq)
+
+# List containers
+# docker container ls
